bfe131e69e0c5c192988acba8434a2a6058970a9
Jeff Hillman <hillmanapps@gmail.com>
2023-12-07 15:43:38 -0700

    Day 6

---
+++ day06/part1.c
@@ -0,0 +1,7 @@
+/* Day 6, part 1 = 3317888 */
+
+#include "race.h"
+
+int main() {
+    printf("%ld", getRaceAnswer(false));
+}
---
+++ day06/part2.c
@@ -0,0 +1,7 @@
+/* Day 6, part 2 = 24655068 */
+
+#include "race.h"
+
+int main() {
+    printf("%ld", getRaceAnswer(true));
+}
