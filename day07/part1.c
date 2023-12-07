f0a8c16a66b06b09a800949eb7afe640cda8240c
Jeff Hillman <hillmanapps@gmail.com>
2023-12-07 15:43:51 -0700

    Day 7

---
+++ day07/part1.c
@@ -0,0 +1,9 @@
+/* Day 7, part 1 = 249483956 */
+
+#include "cards.h"
+
+int main() {
+    printf("%d", getWinnings(false));
+
+    return 0;
+}
---
+++ day07/part2.c
@@ -0,0 +1,9 @@
+/* Day 7, part 2 = 252137472 */
+
+#include "cards.h"
+
+int main() {
+    printf("%d", getWinnings(true));
+
+    return 0;
+}
