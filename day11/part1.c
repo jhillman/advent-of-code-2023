cfd53db5f9081429a8702faa472ee4d990840423
Jeff Hillman <hillmanapps@gmail.com>
2023-12-12 17:59:04 -0700

    Day 11

---
+++ day11/part1.c
@@ -0,0 +1,9 @@
+/* Day 11, part 1 = 9734203 */
+
+#include "expansion.h"
+
+int main() {
+    long answer = galaxyDistanceSum(2);
+
+    printf("%ld", answer);
+}
---
+++ day11/part2.c
@@ -0,0 +1,9 @@
+/* Day 11, part 2 = 568914596391 */
+
+#include "expansion.h"
+
+int main() {
+    long answer = galaxyDistanceSum(1000000);
+
+    printf("%ld", answer);
+}
