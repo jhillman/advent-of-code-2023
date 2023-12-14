2301785f0375e7f6f7732987688383f04257d30c
Jeff Hillman <hillmanapps@gmail.com>
2023-12-13 19:23:38 -0700

    Day 12

---
+++ day12/part1.c
@@ -0,0 +1,12 @@
+/* Day 12, part 1 = 7169 */
+
+#include "springs.h"
+
+int main() {
+    long answer = arrangementCountSum(1);
+
+    printf("%ld", answer);
+
+    return 0;
+}
+
---
+++ day12/part2.c
@@ -0,0 +1,12 @@
+/* Day 12, part 2 = 1738259948652 */
+
+#include "springs.h"
+
+int main() {
+    long answer = arrangementCountSum(5);
+
+    printf("%ld", answer);
+
+    return 0;
+}
+
