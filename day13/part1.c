360febbf8ae3a9bddd2e22bf5bb00293512f2584
Jeff Hillman <hillmanapps@gmail.com>
2023-12-16 10:52:01 -0700

    Day 13

---
+++ day13/part1.c
@@ -0,0 +1,9 @@
+/* Day 13, part 1 = 33735 */
+
+#include "mirrors.h"
+
+int main() {
+    int answer = mirrorLocationScoreSum(false);
+
+    printf("%d", answer);
+}
---
+++ day13/part2.c
@@ -0,0 +1,9 @@
+/* Day 13, part 2 = 38063 */
+
+#include "mirrors.h"
+
+int main() {
+    int answer = mirrorLocationScoreSum(true);
+
+    printf("%d", answer);
+}
