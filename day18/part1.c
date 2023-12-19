c8e81474b9abb43e5f3a9486c79d461c40afb469
Jeff Hillman <hillmanapps@gmail.com>
2023-12-18 20:24:36 -0700

    Day 18

---
+++ day18/part1.c
@@ -0,0 +1,11 @@
+/* Day 18, part 1 = 47139 */
+
+#include "lagoon.h"
+
+int main() {
+    long answer = lagoonVolume(false);
+
+    printf("%ld", answer);
+
+    return 0;
+}
---
+++ day18/part2.c
@@ -0,0 +1,11 @@
+/* Day 18, part 2 = 173152345887206 */
+
+#include "lagoon.h"
+
+int main() {
+    long answer = lagoonVolume(true);
+
+    printf("%ld", answer);
+
+    return 0;
+}
